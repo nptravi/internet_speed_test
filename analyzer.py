687c5b7c371db12307b20fd2ea08e1d3d005e671
nptravi@gmail.com <nptravi@gmail.com>
2025-11-01 17:51:50 +0545

    Dashboards metrics added

--- analyzer.py
+++ analyzer.py
@@ -9,7 +9,35 @@ def get_lastest():
     latest = dt.sort_values(by=["timestamp"], ascending=False, inplace=False).iloc[0].to_dict()
     latest["timestamp"] = latest["timestamp"].to_pydatetime()
     return latest
+def get_results():
+    dt = pd.read_csv(CSV_PATH, parse_dates=["timestamp"])
+    latest = dt.sort_values(by=["timestamp"], ascending=True, inplace=False).iloc[-1].to_dict()
+    latest["timestamp"] = latest["timestamp"].to_pydatetime()
+    results = {"latest": latest}
+    download_speeds = dt.set_index("timestamp")["download_speed_mbps"].to_dict()
+    upload_speeds = dt.set_index("timestamp")["upload_speed_mbps"].to_dict()
+    ping_ms = dt.set_index("timestamp")["ping_ms"].to_dict()
+    results["download_speeds"] = {
+        "average": sum(download_speeds.values()) / len(download_speeds.values()),
+        "max": max(download_speeds.values()),
+        "min": min(download_speeds.values()),
+        "latest": latest["download_speed_mbps"],
+        "values": download_speeds.values()
+    }
+    results["upload_speeds"] = {
+        "average": sum(upload_speeds.values()) / len(upload_speeds.values()),
+        "max": max(upload_speeds.values()),
+        "min": min(upload_speeds.values()),
+        "latest": latest["upload_speed_mbps"]
+    }
+    results["ping_ms"] = {
+        "average": sum(ping_ms.values()) / len(ping_ms.values()),
+        "max": max(ping_ms.values()),
+        "min": min(ping_ms.values()),
+        "latest": latest["ping_ms"]
+    }
+    return results
 
 if __name__ == "__main__":
-    print(get_lastest())
+    print(get_results())
 
--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import analyzer
 
 st.title("Internet Speed Test")
-
+speedtest_result = analyzer.get_results()
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Download Speed (Mbps)", value=speedtest_result["download_speeds"]["latest"],
+          delta=round(speedtest_result["download_speeds"]["latest"]
+                - speedtest_result["download_speeds"]["average"],2), border=True,
+          chart_data = speedtest_result["download_speeds"]["values"])
+col2.metric(label="Upload Speed (Mbps)", value=speedtest_result["upload_speeds"]["latest"],
+          delta=round(speedtest_result["upload_speeds"]["latest"]
+                - speedtest_result["upload_speeds"]["average"],2), border=True)
+col3.metric(label="Ping (ms)", value=speedtest_result["ping_ms"]["latest"],
+          delta=round(speedtest_result["ping_ms"]["latest"]
+                - speedtest_result["ping_ms"]["average"],2), border=True)
